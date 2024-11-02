51d5d7f076560de689aedd9d089dc481bf132316
Mitra HSH <mitrahooshmand4824@gmail.com>
2024-11-02 11:00:35 +0330

    initial pages has established

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
 export default function App() {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </>
+  );
+}
+
+function Logo() {
+  return <h1> Far Away🌴🌊</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">list</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em>you have X items in your list</em>
+    </footer>
+  );
 }
